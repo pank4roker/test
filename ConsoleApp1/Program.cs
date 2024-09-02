5b08e72fb6324e88a34788947a77d0dae2b402ad
Markow Kirill <kirillmarkor@gmail.com>
2024-09-02 19:28:17 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.WriteLine("Введите имя и фамилию: ");
+            string nameSurname = Console.ReadLine();
+            NameSurname(nameSurname);
+            Console.ReadKey();
+        }
+        public static void NameSurname(string nameSurname)
+        {
+            Console.WriteLine(nameSurname);
+        }
+    }
+}
